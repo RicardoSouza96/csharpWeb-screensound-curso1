7e4fcd4f12b88e123c4fff5f0972e9a5ba5997b4
Ricardo Souza <ricksouz96@gmail.com>
2024-08-21 21:15:34 -0300

    Cria o método para recuperar no banco de dados os Artistas e inseri-los em uma lista

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -4,9 +4,13 @@ using ScreenSound.Modelos;
 
 try
 {
-    using var connection = new Connection().ObterConexao();
-    connection.Open();
-    Console.WriteLine(connection.State);
+    var connection = new Connection();
+    var listaArtistas = connection.Listar();
+
+    foreach ( var artist in listaArtistas )
+    {
+        Console.WriteLine(artist);
+    }
 }
 catch(Exception ex)
 {
