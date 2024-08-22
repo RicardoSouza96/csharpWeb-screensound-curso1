1a283881b61111e86fb2dabb46dbf1dcf8fb50bb
Ricardo Souza <ricksouz96@gmail.com>
2024-08-21 21:41:38 -0300

    Refatora a classe Connection para que ela fique responsável apenas pela conexão com o banco

--- ScreenSound/Banco/Connection.cs
+++ ScreenSound/Banco/Connection.cs
@@ -6,38 +6,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScreenSound.Banco
+namespace ScreenSound.Banco;
+
+internal class Connection
 {
-    internal class Connection
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    public SqlConnection ObterConexao()
     {
-        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-
-        public SqlConnection ObterConexao()
-        {
-            return new SqlConnection(connectionString);
-        }
-
-        public IEnumerable<Artista> Listar()
-        {
-            var lista = new List<Artista>();
-            using var connection = ObterConexao();
-            connection.Open();
-
-            string sql = "SELECT * FROM Artistas";
-            SqlCommand command = new SqlCommand(sql, connection);
-            using SqlDataReader dataReader = command.ExecuteReader();
-
-            while (dataReader.Read())
-            {
-                string nomeArtista = Convert.ToString(dataReader["Nome"]);
-                string bioArtista = Convert.ToString(dataReader["Bio"]);
-                int idArtista = Convert.ToInt32(dataReader["ID"]);
-                Artista artista = new(nomeArtista, bioArtista) { Id = idArtista};
-                
-                lista.Add(artista);
-            }
-
-            return lista;
-        }
+        return new SqlConnection(connectionString);
     }
 }
