0088d6d7a8ce8a1f086589bc6f8f9fbce0b569c0
Ricardo Souza <ricksouz96@gmail.com>
2024-08-26 20:38:29 -0300

    Cria a classe MusicaDAL com os respectivos métodos relaciondos à classe Musica

---
+++ ScreenSound/Banco/MusicaDAL.cs
@@ -0,0 +1,35 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco;
+
+internal class MusicaDAL
+{
+    private readonly ScreenSoundContext context;
+
+    public MusicaDAL(ScreenSoundContext context)
+    {
+        this.context = context;
+    }
+
+    public IEnumerable<Musica> Listar()
+    {
+        return context.Musica.ToList();
+    }
+
+    public void Adicionar(Musica musica)
+    {
+        context.Musica.Add(musica);
+        context.SaveChanges();
+    }
+
+    public void Deletar(Musica musica)
+    {
+        context.Musica.Remove(musica);
+        context.SaveChanges();
+    }
+}
