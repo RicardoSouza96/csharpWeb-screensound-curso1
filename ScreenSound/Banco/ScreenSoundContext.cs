178809e3d7b995fa128a2d14c8624b1b17cb13e9
Ricardo Souza <ricksouz96@gmail.com>
2024-08-26 20:34:20 -0300

    Cria o DbSet para a tabela de musicas

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -6,12 +6,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+    
 namespace ScreenSound.Banco;
 
 internal class ScreenSoundContext : DbContext
 {
     public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musica { get; set; }
 
     private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
