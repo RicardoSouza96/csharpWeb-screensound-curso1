0338b4487623d30e24f95279ffda07be61ab6566
Ricardo Souza <ricksouz96@gmail.com>
2024-09-06 21:20:58 -0300

    Cria migration para adicionar na tabela Musica o ID do artista

---
+++ ScreenSound/Migrations/20240907001204_InserirArtistaEmMusica.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class InserirArtistaEmMusica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Musica SET ArtistaId = 1 WHERE Id = 1");
+            migrationBuilder.Sql("UPDATE Musica SET ArtistaId = 4 WHERE Id = 4");
+            migrationBuilder.Sql("UPDATE Musica SET ArtistaId = 2 WHERE Id = 2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = NULL");
+        }
+    }
+}
